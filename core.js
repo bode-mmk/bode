a748f605df649968effbd4a493f34979008189ed
nahanahaha-legend <super.perl.user@gmail.com>
2018-08-11 03:10:05 +0900

    initial commit

---
+++ core.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.leftside', {
+  easing: 'ease',
+  duration: 2000,
+  distance: '150px',
+  opacity: 1, 
+  scale: 1, 
+  origin:"left"
+  });
+
+var option = {
+  section : '.js-section', //対象の指定
+  easing: "swing", // イージングの指定
+  scrollSpeed: 600, // スクロール速度
+  scrollbars: true, // スクロールバーを表示するか
+};
+
+$(function(){
+  $.scrollify(option);  // scrollifyの実行
+});
+
+function copy_to_clipboard(text){
+  var target = document.createElement("textarea");
+  target.value = text;
+  document.body.appendChild(target);
+  target.select();
+  document.execCommand('copy');
+  target.parentElement.removeChild(target);
+}
