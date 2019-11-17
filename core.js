8a5c920c26e266142bbc6ed109d0da8801d1e1f5
nahanahaha-legend <super.perl.user@gmail.com>
2019-11-17 22:31:49 +0900

    modified profile

--- core.js
+++ core.js
@@ -1,25 +1,17 @@
-window.sr = ScrollReveal({reset: true});
-sr.reveal('.leftside', {
-  easing: 'ease',
-  duration: 2000,
-  distance: '150px',
-  opacity: 1, 
-  scale: 1, 
-  origin:"left"
+$(function(){
+  var _window = $(window),
+      _header = $('.page-header'),
+      backBottom;
+  
+  _window.on('scroll',function(){
+      backBottom = $('.back').height();
+      if(_window.scrollTop() > backBottom){
+          _header.addClass('transform');   
+      }
+      else{
+          _header.removeClass('transform');   
+      }
   });
-
-var option = {
-  section : '.js-section', //対象の指定
-  easing: "swing", // イージングの指定
-  scrollSpeed: 600, // スクロール速度
-  scrollbars: true, // スクロールバーを表示するか
-};
-
-function copy_to_clipboard(text){
-  var target = document.createElement("textarea");
-  target.value = text;
-  document.body.appendChild(target);
-  target.select();
-  document.execCommand('copy');
-  target.parentElement.removeChild(target);
-}
+  
+  _window.trigger('scroll');
+});
